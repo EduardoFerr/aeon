0d3a6790f05ffa00b1da8c705283c06a517b0d42
Eduardo Ferreira <ferr.tutorial@gmail.com>
2025-01-26 17:54:47 -0300

    adicionado hash

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,17 @@ export default defineConfig({
     host: '127.0.0.1',
     port: 3000,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith('.css')) {
+            return 'assets/css/[name]-[hash][extname]'; // Adiciona hash nos arquivos CSS
+          }
+          return 'assets/[name]-[hash][extname]'; // Para outros assets
+        },
+      },
+    },
+    cssCodeSplit: true, // Certifique-se de que o CSS seja separado em arquivos
+  },
 });
