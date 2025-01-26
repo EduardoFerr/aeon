b9272b193225030a1d5a92074e18beaf944d1ece
Eduardo Ferreira <ferr.tutorial@gmail.com>
2025-01-25 22:24:13 -0300

    update

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -4,7 +4,7 @@ export class HomeScreen extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="home-screen">
-                <h1>Bem-vindo ao App de Tarot</h1>
+                <h1>Livro de Thoth</h1>
                 <button onclick="navigateTo('all-cards')">Ver Todas as Cartas</button>
                 <button onclick="navigateTo('three-card-game')">Jogo das 3 Cartas</button>
                 <button onclick="navigateTo('history')">Ver Histórico</button>
