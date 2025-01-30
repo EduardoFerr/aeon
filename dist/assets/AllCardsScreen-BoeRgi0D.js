d160aca530cd7ebed896402faebcb1a4c853a91b
Eduardo Ferreira <ferr.tutorial@gmail.com>
2025-01-29 21:29:06 -0300

    sasha2

--- dist/assets/AllCardsScreen-BoeRgi0D.js
+++ dist/assets/AllCardsScreen-BoeRgi0D.js
@@ -1,9 +1,8 @@
-import{_ as s}from"./index-DOz7LL4m.js";import{l as o}from"./cardsUtil-H_foYc3n.js";s(()=>import("./TarotCard-BhMfcB2C.js"),[]);class c extends HTMLElement{async connectedCallback(){this.innerHTML=`
+import{_ as o}from"./index-D_NjZ1G0.js";import{l as s}from"./cardsUtil-H_foYc3n.js";o(()=>import("./TarotCard-BhMfcB2C.js"),[]);class c extends HTMLElement{async connectedCallback(){this.innerHTML=`
             <div class="all-cards-screen">
-                <h1>Todas as Cartas</h1>
                 <div class="card-container" id="all-cards"></div>
                 <button onclick="navigateTo('home')">Voltar</button>
             </div>
-        `,await this.renderCards()}async renderCards(){try{const r=await o();r.sort((a,t)=>a.order-t.order);const e=this.querySelector("#all-cards");e.innerHTML=r.map(a=>`
+        `,document.querySelector("header>h1").textContent="Todas as Cartas",await this.renderCards()}async renderCards(){try{const r=await s();r.sort((a,t)=>a.order-t.order);const e=this.querySelector("#all-cards");e.innerHTML=r.map(a=>`
                         <tarot-card data-card='${JSON.stringify(a)}'></tarot-card>
                     `).join("")}catch(r){console.error("Erro ao carregar as cartas:",r)}}}customElements.define("all-cards-screen",c);export{c as AllCardsScreen};
