742a276fbc71cf5794e3553f87e7141c0986e802
Eduardo Ferreira <ferr.tutorial@gmail.com>
2025-01-31 01:01:39 -0300

    manifest10

--- vite.config.js
+++ vite.config.js
@@ -1,56 +1,61 @@
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import { partytownVite } from "@builder.io/partytown/utils";
+import path from "node:path";
+import fs from "node:fs/promises";
 
 export default defineConfig({
-  server: {
-    open: true,
-    host: '127.0.0.1',
-    port: 3000,
-  },
   build: {
-    base: '/',
+    base: "/",
     manifest: true,
     rollupOptions: {
-      output: {
-        assetFileNames: ({ name }) => {
-          if (name && name.endsWith('.css')) {
-            return 'assets/css/[name]-[hash][extname]';
-          }
-          if (name === 'manifest.json' || name === 'favicon.ico') {
-            return '[name][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
-        },
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        sobre: path.resolve(__dirname, "sobre.html"),
+        contato: path.resolve(__dirname, "contato.html"),
+        livro: path.resolve(__dirname, "livro.html"),
+        "politica-privacidade": path.resolve(
+          __dirname,
+          "politica-privacidade.html"
+        ),
       },
     },
     cssCodeSplit: true,
   },
   plugins: [
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'icons/*.png'],
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        sourcemap: true
+      },
+      includeAssets: ["favicon.ico", "robots.txt", "icons/*.png"],
       manifest: {
-        name: 'Aeon Fool',
-        short_name: 'Aeon Fool',
-        description: 'Explorando o Tarot de Thoth, de Aleister Crowley, com leituras e sabedoria esotérica.',
-        start_url: '/index.html',
-        scope: '/',
-        display: 'standalone',
-        theme_color: '#000000',
-        background_color: '#000000',
+        name: "Aeon Fool",
+        short_name: "Aeon Fool",
+        description:
+          "Explorando o Tarot de Thoth, de Aleister Crowley, com leituras e sabedoria esotérica.",
+        start_url: "/index.html",
+        scope: "/",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#000000",
         icons: [
           {
-            src: '/icons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: '/icons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
+            src: "/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+    partytownVite({
+      dest: path.resolve(__dirname, "dist/~partytown"),
+    }),
+  ],
 });
